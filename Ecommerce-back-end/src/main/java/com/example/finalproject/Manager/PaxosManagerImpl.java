5e60b2ea2096925faff8294cc2757e664f211b96
buithuytien <buithuytien13.13@gmail.com>
2023-04-23 23:43:53 -0400

    add logger

--- Ecommerce-back-end/src/main/java/com/example/finalproject/Manager/PaxosManagerImpl.java
+++ Ecommerce-back-end/src/main/java/com/example/finalproject/Manager/PaxosManagerImpl.java
@@ -2,6 +2,7 @@ package com.example.finalproject.Manager;
 
 import com.example.finalproject.Model.ForwardRequestRepr;
 import com.example.finalproject.Model.Promise;
+import com.example.finalproject.Resource.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 
@@ -11,38 +12,39 @@ public class PaxosManagerImpl implements PaxosManager{
     private ForwardRequestRepr acceptedValue = null;
     private Long acceptedProposal = -1L;
     private boolean alreadyAccepted = false;
+    private MyLogger myLogger = MyLogger.getInstance();
 
     @Override
     public Promise prepare(Long currentProposal) {
-        System.out.println("PAxos before prepare: Current proposal: " + currentProposal);
+        myLogger.info("PAxos before prepare: Current proposal: " + currentProposal);
         if (currentProposal < this.acceptedProposal) {
             return null;
         }
 
-        System.out.println("Paxos prepare update proposal to Current proposal: " + currentProposal);
+        myLogger.info("Paxos prepare update proposal to Current proposal: " + currentProposal);
         this.acceptedProposal = currentProposal;
 
         Promise res;
 
         if (this.alreadyAccepted) {
             res = new Promise(this.acceptedProposal, this.acceptedValue).promise().accept();
-            System.out.println("Paxos prepare return Promise: " + res);
+            myLogger.info("Paxos prepare return Promise: " + res);
         }
         res = new Promise(this.acceptedProposal, null).promise();
-        System.out.println("Paxos prepare return Promise: " + res);
+        myLogger.info("Paxos prepare return Promise: " + res);
         return res;
     }
 
     @Override
     public Promise accept(Long currentProposal, ForwardRequestRepr request) {
-        System.out.println("Paxos accept: Current proposal: " + currentProposal + " Accepted proposal: " + this.acceptedProposal);
+        myLogger.info("Paxos accept: Current proposal: " + currentProposal + " Accepted proposal: " + this.acceptedProposal);
         if (currentProposal.equals(this.acceptedProposal)) {
             this.alreadyAccepted = true;
             this.acceptedProposal = currentProposal;
             this.acceptedValue = request;
             Promise res;
             res = new Promise(this.acceptedProposal, this.acceptedValue).promise().accept(); // already prepared in the prepare method, thus .promise() is not needed
-            System.out.println("Paxos accept return Promise: " + res);
+            myLogger.info("Paxos accept return Promise: " + res);
             return res;
         }
         return null;
