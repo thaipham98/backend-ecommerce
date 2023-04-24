9d264b5b9117bce04b40fa3a97f582200f042d6b
Brian Nguyen <bachnguyen91097@gmail.com>
2023-04-24 05:16:06 -0400

    Changes by giath - Kafka Main App

--- Ecommerce-back-end/src/main/java/com/example/finalproject/MainApplication.java
+++ Ecommerce-back-end/src/main/java/com/example/finalproject/MainApplication.java
@@ -2,8 +2,12 @@ package com.example.finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableKafka
 public class MainApplication {
 
     public static void main(String[] args) {
